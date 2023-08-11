61183476d3fa841e53202cb6c4e277314acecc64
jay <jayrchauhan007@gmail.com>
2023-08-11 11:54:31 +0530

    vgfevfghv

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// const express =require('express')
-// const app=express()
-// const port=process.env.port||5001;
 
-// app.get('/',(req,res)=>{
-//     res.send('ok')
-// })
-
-// app.get('services/service1',(req,res)=>{
-//     res.send('ok')
-// })
-
-
-// app.get('/',(req,res)=>{
-//     res.send('ok')
-// })
+const argvs = process.argv
+const argv = argvs.slice(2)
+const operation = argv[0]
+const operator1 = parseInt(argv[1])
+const operator2 = parseInt(argv[2])
+ 
+if (operation === 'add') {
+    console.log(operation + ' is '
+        + (operator1 + operator2));
+}
+if (operation === 'subtract') {
+    console.log(operation + ' is '
+        + (operator1 - operator2));
+}
+if (operation === 'multiply') {
+    console.log(operation + ' is '
+        + (operator1 + operator2));
+}
+if (operation === 'divide') {
+    console.log(operation + ' is '
+        + (operator1 - operator2));
+}
