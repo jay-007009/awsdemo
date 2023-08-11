7df601a3cfc1e30ee3a0cf90d2adb80c8b248a24
jay <jayrchauhan007@gmail.com>
2023-08-11 11:53:10 +0530

    bvnhjbvhjr

---
+++ index.js
@@ -0,0 +1,16 @@
+// const express =require('express')
+// const app=express()
+// const port=process.env.port||5001;
+
+// app.get('/',(req,res)=>{
+//     res.send('ok')
+// })
+
+// app.get('services/service1',(req,res)=>{
+//     res.send('ok')
+// })
+
+
+// app.get('/',(req,res)=>{
+//     res.send('ok')
+// })
